3ed06352b78de76b8bcba6612d3d543b63a0da95
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 12:17:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 def reverse_each_word(sentence)
   elements = sentence.split " "
-  sentence1 = elements.map {|word| "#{word.reverse}"}
+  sentence1 = elements.collect {|word| "#{word.reverse}"}
   reversedSentence = sentence1.join " "
-
 end
+
