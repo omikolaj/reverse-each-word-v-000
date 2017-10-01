b5598254d2c0df10b888f1c6d3573e5e644a9277
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-01 11:58:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,9 +3,5 @@ def reverse_each_word(sentence)
   elements = sentence.split " "
   elements.each {|word| items += "#{word.reverse}.join"}
   puts items
-end
-
-
-def reverse_each_word(sentence)
-
+  binding.pry
 end
